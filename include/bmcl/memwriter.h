3908cc5faa2855254184721b9bc789465d49b898
Zalevskiy.Danila <dndanik@gmail.com>
2014-10-04 22:48:36 +0400

    Tidy up memwriter

--- include/bmcl/memwriter.h
+++ include/bmcl/memwriter.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
     uint8_t* start;
     uint8_t* current;
@@ -24,18 +28,26 @@ size_t memwriter_max_size(const memwriter_t* self);
 
 size_t memwriter_size_left(const memwriter_t* self);
 
-void memwriter_append(memwriter_t* self, const void* data, size_t size);
+void memwriter_write(memwriter_t* self, const void* data, size_t size);
 
 void memwriter_fill(memwriter_t* self, uint8_t byte, size_t size);
 
 void memwriter_fillup(memwriter_t* self, uint8_t byte);
 
-void memwriter_append_uint8(memwriter_t* self, uint8_t value);
+void memwriter_write_uint8(memwriter_t* self, uint8_t value);
 
-void memwriter_append_uint16(memwriter_t* self, uint16_t value);
+void memwriter_write_uint16le(memwriter_t* self, uint16_t value);
 
-void memwriter_append_uint16be(memwriter_t* self, uint16_t value);
+void memwriter_write_uint32le(memwriter_t* self, uint32_t value);
 
-void memwriter_append_uint32be(memwriter_t* self, uint32_t value);
+void memwriter_write_uint64le(memwriter_t* self, uint64_t value);
 
-void memwriter_append_uint64be(memwriter_t* self, uint64_t value);
+void memwriter_write_uint16be(memwriter_t* self, uint16_t value);
+
+void memwriter_write_uint32be(memwriter_t* self, uint32_t value);
+
+void memwriter_write_uint64be(memwriter_t* self, uint64_t value);
+
+#ifdef __cplusplus
+}
+#endif
--- src/memwriter.c
+++ src/memwriter.c
@@ -1,12 +1,11 @@
+#include "bmcl/endian.h"
+#include "bmcl/memwriter.h"
+
 #include <stdint.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
-
-#include "bmcl/endian.h"
-#include "bmcl/memwriter.h"
 
 void memwriter_init(memwriter_t* self, void* dest, size_t max_size)
 {
@@ -45,7 +44,7 @@ size_t memwriter_size_left(const memwriter_t* self)
     return self->end - self->current;
 }
 
-void memwriter_append(memwriter_t* self, const void* data, size_t size)
+void memwriter_write(memwriter_t* self, const void* data, size_t size)
 {
     assert(memwriter_size_left(self) >= size);
     memcpy(self->current, data, size);
@@ -66,37 +65,51 @@ void memwriter_fillup(memwriter_t* self, uint8_t byte)
     self->current += size;
 }
 
-void memwriter_append_uint8(memwriter_t* self, uint8_t value)
+void memwriter_write_uint8(memwriter_t* self, uint8_t value)
 {
     assert(memwriter_size_left(self) >= 1);
     *self->current = value;
     self->current++;
 }
 
-void memwriter_append_uint16(memwriter_t* self, uint16_t value)
+void memwriter_write_uint16le(memwriter_t* self, uint16_t value)
 {
     assert(memwriter_size_left(self) >= 2);
-    *(uint16_t*)self->current = value;
+    le16enc(self->current, value);
     self->current += 2;
 }
 
-void memwriter_append_uint16be(memwriter_t* self, uint16_t value)
-{
-    assert(memwriter_size_left(self) >= 2);
-    *(uint16_t*)self->current = htobe16(value);
-    self->current += 2;
-}
-
-void memwriter_append_uint32be(memwriter_t* self, uint32_t value)
+void memwriter_write_uint32le(memwriter_t* self, uint32_t value)
 {
     assert(memwriter_size_left(self) >= 4);
-    *(uint32_t*)self->current = htobe32(value);
+    le32enc(self->current, value);
     self->current += 4;
 }
 
-void memwriter_append_uint64be(memwriter_t* self, uint64_t value)
+void memwriter_write_uint64le(memwriter_t* self, uint64_t value)
 {
     assert(memwriter_size_left(self) >= 8);
-    *(uint64_t*)self->current = htobe64(value);
+    le64enc(self->current, value);
+    self->current += 8;
+}
+
+void memwriter_write_uint16be(memwriter_t* self, uint16_t value)
+{
+    assert(memwriter_size_left(self) >= 2);
+    be16enc(self->current, value);
+    self->current += 2;
+}
+
+void memwriter_write_uint32be(memwriter_t* self, uint32_t value)
+{
+    assert(memwriter_size_left(self) >= 4);
+    be32enc(self->current, value);
+    self->current += 4;
+}
+
+void memwriter_write_uint64be(memwriter_t* self, uint64_t value)
+{
+    assert(memwriter_size_left(self) >= 8);
+    be64enc(self->current, value);
     self->current += 8;
 }
