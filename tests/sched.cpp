f7395ad74c5f7380893f810108c73b77465a03d6
dndanik <dndanik@gmail.com>
2014-10-29 21:37:49 +0300

    Update sched_t tests

--- tests/sched.cpp
+++ tests/sched.cpp
@@ -13,12 +13,19 @@ protected:
     }
 };
 
+struct UserData {
+    uint64_t max_count;
+    uint64_t exec_count;
+    size_t counters[4];
+};
+
 static bool executor1(void* fp, void* userData)
 {
-    size_t* data = (size_t*)userData;
+    UserData* data = (UserData*)userData;
     void (*func)(size_t*) = (void (*)(size_t*))fp;
-    func(data);
-    return true;
+    func(data->counters);
+    data->exec_count++;
+    return data->exec_count < data->max_count;
 }
 
 static void inc1(size_t* counter)
@@ -46,13 +53,12 @@ TEST_F(SchedTest, test1)
     sched_t* sched;
     const task_t tasks[4] = {{(void*)inc1, 3}, {(void*)inc2, 11}, {(void*)inc3, 101}, {(void*)inc4, 211}};
     sched = sched_create(tasks, 4, executor1);
-    size_t userData[4] = {0, 0, 0, 0};
-    for (int i = 0; i < 11 * 101 * 211 + 3 * 101 * 211 + 3 * 11 * 211 + 3 * 11 * 101; i++) {
-        sched_exec_next(sched, userData);
-    }
-    EXPECT_EQ(11 * 101 * 211, userData[0]);
-    EXPECT_EQ(3 * 101 * 211, userData[1]);
-    EXPECT_EQ(3 * 11 * 211, userData[2]);
-    EXPECT_EQ(3 * 11 * 101, userData[3]);
+    uint64_t max_count = 11 * 101 * 211 + 3 * 101 * 211 + 3 * 11 * 211 + 3 * 11 * 101;
+    UserData userData = {max_count, 0, {0, 0, 0, 0}};
+    sched_exec_while_possible(sched, &userData);
+    EXPECT_EQ(11 * 101 * 211, userData.counters[0]);
+    EXPECT_EQ(3 * 101 * 211, userData.counters[1]);
+    EXPECT_EQ(3 * 11 * 211, userData.counters[2]);
+    EXPECT_EQ(3 * 11 * 101, userData.counters[3]);
     sched_destroy(sched);
 }
