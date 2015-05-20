2d3ebe87b43018b1cd2f08d12ae45f22b57e156e
dndanik <dndanik@gmail.com>
2015-05-20 21:07:23 +0300

    Add Either<T, E>

---
+++ src/bmcl/Either.h
@@ -0,0 +1,229 @@
+#pragma once
+
+#include <cassert>
+#include <utility>
+
+namespace bmcl {
+
+template <typename T, typename E>
+class Either {
+public:
+    Either(const T& first);
+    Either(T&& first);
+    Either(const E& second);
+    Either(E&& second);
+    Either(const Either& other);
+    Either(Either&& other);
+    ~Either();
+
+    bool isFirst() const;
+    bool isSecond() const;
+    const T& unwrapFirst() const;
+    T& unwrapFirst();
+    const E& unwrapSecond() const;
+    E& unwrapSecond();
+
+    //TODO: op=
+    Either& operator=(const Either& other);
+    Either& operator=(Either&& other);
+//     Either& operator=(const T& first);
+//     Either& operator=(T&& first);
+//     Either& operator=(const E& second);
+//     Either& operator=(E&& second);
+
+private:
+    const T* asFirst() const;
+    T* asFirst();
+    const E* asSecond() const;
+    E* asSecond();
+
+#ifdef _MSC_VER
+    struct TEContainer {
+        T t;
+        E e;
+    };
+
+    typename std::aligned_union<sizeof(TEContainer), T, E>::type _data;
+#else
+    template <typename S>
+    static constexpr S max(S t, S r)
+    {
+        return t > r ? t : r;
+    }
+
+    typename std::aligned_storage<max(sizeof(T), sizeof(E)), max(alignof(T), alignof(E))>::type _data;
+#endif
+    bool _isFirst;
+};
+
+template <typename T, typename E>
+inline const T* Either<T, E>::asFirst() const
+{
+    return reinterpret_cast<const T*>(&_data);
+}
+
+template <typename T, typename E>
+inline T* Either<T, E>::asFirst()
+{
+    return reinterpret_cast<T*>(&_data);
+}
+
+template <typename T, typename E>
+inline const E* Either<T, E>::asSecond() const
+{
+    return reinterpret_cast<const E*>(&_data);
+}
+
+template <typename T, typename E>
+inline E* Either<T, E>::asSecond()
+{
+    return reinterpret_cast<E*>(&_data);
+}
+
+template <typename T, typename E>
+inline Either<T, E>::Either(const T& value)
+    : _isFirst(true)
+{
+    new (asFirst()) T(value);
+}
+
+template <typename T, typename E>
+inline Either<T, E>::Either(T&& value)
+    : _isFirst(true)
+{
+    new (asFirst()) T(std::move(value));
+}
+
+template <typename T, typename E>
+inline Either<T, E>::Either(const E& error)
+    : _isFirst(false)
+{
+    new (asSecond()) E(error);
+}
+
+template <typename T, typename E>
+inline Either<T, E>::Either(E&& error)
+    : _isFirst(false)
+{
+    new (asSecond()) E(std::move(error));
+}
+
+template <typename T, typename E>
+inline Either<T, E>::Either(const Either<T, E>& other)
+{
+    _isFirst = other._isFirst;
+    if (_isFirst) {
+        new (asFirst()) T(*other.asFirst());
+    } else {
+        new (asSecond()) E(*other.asSecond());
+    }
+}
+
+template <typename T, typename E>
+inline Either<T, E>::Either(Either<T, E>&& other)
+{
+    _isFirst = other._isFirst;
+    if (_isFirst) {
+        new (asFirst()) T(std::move(*other.asFirst()));
+    } else {
+        new (asSecond()) E(std::move(*other.asSecond()));
+    }
+}
+
+template <typename T, typename E>
+inline Either<T, E>::~Either()
+{
+    if (_isFirst) {
+        asFirst()->~T();
+    } else {
+        asSecond()->~E();
+    }
+}
+
+template <typename T, typename E>
+inline bool Either<T, E>::isFirst() const
+{
+    return _isFirst;
+}
+
+template <typename T, typename E>
+inline bool Either<T, E>::isSecond() const
+{
+    return !_isFirst;
+}
+
+template <typename T, typename E>
+inline const T& Either<T, E>::unwrapFirst() const
+{
+    assert(_isFirst);
+    return *asFirst();
+}
+
+template <typename T, typename E>
+inline T& Either<T, E>::unwrapFirst()
+{
+    assert(_isFirst);
+    return *asFirst();
+}
+
+template <typename T, typename E>
+inline const E& Either<T, E>::unwrapSecond() const
+{
+    assert(!_isFirst);
+    return *asSecond();
+}
+
+template <typename T, typename E>
+inline E& Either<T, E>::unwrapSecond()
+{
+    assert(!_isFirst);
+    return *asSecond();
+}
+
+
+template <typename T, typename E>
+inline Either<T, E>& Either<T, E>::operator=(const Either<T, E>& other)
+{
+    if (other._isFirst) {
+        if (_isFirst) {
+            *asFirst() = *other.asFirst();
+        } else {
+            _isFirst = true;
+            asSecond()->~E();
+            new (asFirst()) T(*other.asFirst());
+        }
+    } else {
+        if (_isFirst) {
+            _isFirst = false;
+            asFirst()->~T();
+            new (asSecond()) E(*other.asSecond());
+        } else {
+            *asSecond() = std::move(*other.asSecond());
+        }
+    }
+    return *this;
+}
+
+template <typename T, typename E>
+inline Either<T, E>& Either<T, E>::operator=(Either<T, E>&& other)
+{
+    if (other._isFirst) {
+        if (_isFirst) {
+            *asFirst() = std::move(*other.asFirst());
+        } else {
+            _isFirst = true;
+            asSecond()->~E();
+            new (asFirst()) T(std::move(*other.asFirst()));
+        }
+    } else {
+        if (_isFirst) {
+            _isFirst = false;
+            asFirst()->~T();
+            new (asSecond()) E(std::move(*other.asSecond()));
+        } else {
+            *asSecond() = std::move(*other.asSecond());
+        }
+    }
+    return *this;
+}
+}
---
+++ tests/Either.cpp
@@ -0,0 +1,126 @@
+#include "bmcl/Either.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+using namespace bmcl;
+
+TEST(EitherTest, createCopySecond)
+{
+    std::string errString = "second";
+    Either<int, std::string> either(errString);
+    EXPECT_FALSE(either.isFirst());
+    EXPECT_TRUE(either.isSecond());
+    EXPECT_EQ("second", either.unwrapSecond());
+}
+
+TEST(EitherTest, createMoveSecond)
+{
+    std::string errString = "second";
+    Either<int, std::string> either(std::move(errString));
+    EXPECT_FALSE(either.isFirst());
+    EXPECT_TRUE(either.isSecond());
+    EXPECT_EQ("second", either.unwrapSecond());
+}
+
+TEST(EitherTest, createCopyEither)
+{
+    std::vector<int> data({1, 2, 3, 4, 6});
+    Either<std::vector<int>, std::string> either(data);
+    EXPECT_TRUE(either.isFirst());
+    EXPECT_FALSE(either.isSecond());
+    EXPECT_EQ(std::vector<int>({1, 2, 3, 4, 6}), either.unwrapFirst());
+}
+
+TEST(EitherTest, createMoveEither)
+{
+    std::vector<int> data({1, 2, 3, 4, 5});
+    Either<std::vector<int>, std::string> either(std::move(data));
+    EXPECT_TRUE(either.isFirst());
+    EXPECT_FALSE(either.isSecond());
+    EXPECT_EQ(std::vector<int>({1, 2, 3, 4, 5}), either.unwrapFirst());
+}
+
+TEST(EitherTest, copyConstructSecond)
+{
+    std::vector<int> err{3, 2, 1};
+    Either<std::string, std::vector<int>> oldEither(err);
+    Either<std::string, std::vector<int>> newEither(oldEither);
+    EXPECT_FALSE(newEither.isFirst());
+    EXPECT_TRUE(newEither.isSecond());
+    EXPECT_EQ(std::vector<int>({3, 2, 1}), newEither.unwrapSecond());
+}
+
+TEST(EitherTest, copyConstructEither)
+{
+    Either<std::string, int> oldEither("either");
+    Either<std::string, int> newEither(oldEither);
+    EXPECT_TRUE(newEither.isFirst());
+    EXPECT_FALSE(newEither.isSecond());
+    EXPECT_EQ("either", newEither.unwrapFirst());
+}
+
+TEST(EitherTest, moveConstructSecond)
+{
+    std::vector<int> err{3, 2, 1};
+    Either<std::string, std::vector<int>> oldEither(err);
+    Either<std::string, std::vector<int>> newEither(std::move(oldEither));
+    EXPECT_FALSE(newEither.isFirst());
+    EXPECT_TRUE(newEither.isSecond());
+    EXPECT_EQ(std::vector<int>({3, 2, 1}), newEither.unwrapSecond());
+}
+
+TEST(EitherTest, moveConstructEither)
+{
+    Either<std::string, int> oldEither("either");
+    Either<std::string, int> newEither(std::move(oldEither));
+    EXPECT_TRUE(newEither.isFirst());
+    EXPECT_FALSE(newEither.isSecond());
+    EXPECT_EQ("either", newEither.unwrapFirst());
+}
+
+TEST(EitherTest, operatorEqFirstFirst)
+{
+    Either<std::string, int> first1("asdasd");
+    Either<std::string, int> first2("qweqwe");
+    first1 = first2;
+    EXPECT_TRUE(first1.isFirst());
+    EXPECT_TRUE(first2.isFirst());
+    EXPECT_EQ("qweqwe", first1.unwrapFirst());
+    EXPECT_EQ("qweqwe", first2.unwrapFirst());
+}
+
+TEST(EitherTest, operatorEqFirstSecond)
+{
+    Either<std::string, int> first("first");
+    Either<std::string, int> second(2);
+    first = second;
+    EXPECT_TRUE(first.isSecond());
+    EXPECT_TRUE(second.isSecond());
+    EXPECT_EQ(2, first.unwrapSecond());
+    EXPECT_EQ(2, second.unwrapSecond());
+}
+
+TEST(EitherTest, operatorEqSecondFirst)
+{
+    Either<std::string, int> second(2);
+    Either<std::string, int> first("first");
+    second = first;
+    EXPECT_TRUE(first.isFirst());
+    EXPECT_TRUE(second.isFirst());
+    EXPECT_EQ("first", first.unwrapFirst());
+    EXPECT_EQ("first", second.unwrapFirst());
+}
+
+TEST(EitherTest, operatorEqSecondSecond)
+{
+    Either<std::string, int> second1(123);
+    Either<std::string, int> second2(768);
+    second1 = second2;
+    EXPECT_TRUE(second1.isSecond());
+    EXPECT_TRUE(second2.isSecond());
+    EXPECT_EQ(768, second1.unwrapSecond());
+    EXPECT_EQ(768, second2.unwrapSecond());
+}
