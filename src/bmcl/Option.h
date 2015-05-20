82a665bb13e7950fd9ba8cec41e9fa02ad7e3ea0
dndanik <dndanik@gmail.com>
2015-05-20 20:45:29 +0300

    Add Option<T>

---
+++ src/bmcl/Option.h
@@ -0,0 +1,340 @@
+#pragma once
+
+#include <utility>
+#include <cassert>
+
+namespace bmcl {
+
+struct NoneType {
+public:
+    NoneType()
+    {
+    }
+};
+
+const NoneType None;
+
+struct InPlaceType {
+public:
+    InPlaceType()
+    {
+    }
+};
+
+const InPlaceType InPlace;
+
+template <typename T>
+class Option {
+public:
+    Option();
+    Option(NoneType);
+    ~Option();
+    Option(const T& value);
+    Option(T&& value);
+    Option(const Option& other);
+    Option(Option&& other);
+
+    template <typename... A>
+    Option(InPlaceType, A&&... args);
+
+    template <typename... A>
+    void emplace(A&&... args);
+
+    bool isSome() const;
+    bool isNone() const;
+
+    const T& unwrap() const;
+    T& unwrap();
+
+    template <class R>
+#ifdef _MSC_VER
+    T unwrapOr(R&& value) const;
+#else
+    T unwrapOr(R&& value) const&;
+
+    template <class R>
+    T unwrapOr(R&& value) && ;
+#endif
+
+    T take();
+
+    Option& operator=(const Option& other);
+    Option& operator=(Option&& other);
+    Option& operator=(const T& value);
+    Option& operator=(T&& value);
+
+    const T* operator->() const;
+    T* operator->();
+    const T& operator*() const;
+    T& operator*();
+
+    operator bool() const;
+
+private:
+    const T* asValue() const;
+    T* asValue();
+#ifdef _MSC_VER
+    // msvc has no support for c++11 unrestricted union
+    typename std::aligned_union<sizeof(T), T>::type _data;
+#else
+    // gcc has no aligned_union
+    typename std::aligned_storage<sizeof(T), alignof(T)>::type _data;
+#endif
+    bool _isSome;
+};
+
+template <typename T>
+inline const T* Option<T>::asValue() const
+{
+    return reinterpret_cast<const T*>(&_data);
+}
+
+template <typename T>
+inline T* Option<T>::asValue()
+{
+    return reinterpret_cast<T*>(&_data);
+}
+
+template <typename T>
+inline Option<T>::Option()
+    : _isSome(false)
+{
+}
+
+template <typename T>
+inline Option<T>::Option(NoneType)
+    : _isSome(false)
+{
+}
+
+template <typename T>
+template <typename... A>
+inline Option<T>::Option(InPlaceType, A&&... args)
+    : _isSome(true)
+{
+    new (asValue()) T(std::forward<A>(args)...);
+}
+
+template <typename T>
+inline Option<T>::~Option()
+{
+    if (_isSome) {
+        asValue()->~T();
+    }
+}
+
+template <typename T>
+inline Option<T>::Option(const T& value)
+    : _isSome(true)
+{
+    new (asValue()) T(value);
+}
+
+template <typename T>
+inline Option<T>::Option(T&& value)
+    : _isSome(true)
+{
+    new (asValue()) T(std::forward<T>(value));
+}
+
+template <typename T>
+inline Option<T>::Option(const Option<T>& other)
+    : _isSome(other._isSome)
+{
+    if (_isSome) {
+        new (asValue()) T(*other.asValue());
+    }
+}
+
+template <typename T>
+inline Option<T>::Option(Option<T>&& other)
+    : _isSome(other._isSome)
+{
+    if (_isSome) {
+        new (asValue()) T(std::move(*other.asValue()));
+        other._isSome = false;
+        other.asValue()->~T();
+    }
+}
+
+template <typename T>
+inline bool Option<T>::isSome() const
+{
+    return _isSome;
+}
+
+template <typename T>
+inline bool Option<T>::isNone() const
+{
+    return !_isSome;
+}
+
+template <typename T>
+inline const T& Option<T>::unwrap() const
+{
+    assert(_isSome);
+    return *asValue();
+}
+
+template <typename T>
+inline T& Option<T>::unwrap()
+{
+    assert(_isSome);
+    return *asValue();
+}
+
+template <typename T>
+template <typename R>
+#ifdef _MSC_VER
+inline T Option<T>::unwrapOr(R&& value) const
+#else
+inline T Option<T>::unwrapOr(R&& value) const &
+#endif
+{
+    if (_isSome) {
+        return unwrap();
+    }
+    return std::forward<R>(value);
+}
+
+#ifndef _MSC_VER
+template <typename T>
+template <typename R>
+inline T Option<T>::unwrapOr(R&& value) &&
+{
+    if (_isSome) {
+        return take();
+    }
+    return std::forward<R>(value);
+}
+#endif
+
+template <typename T>
+inline T Option<T>::take()
+{
+    assert(_isSome);
+    _isSome = false;
+    T data = std::move(*asValue());
+    asValue()->~T();
+    return data;
+}
+
+template <typename T>
+inline Option<T>& Option<T>::operator=(const Option<T>& other)
+{
+    if (other._isSome) {
+        if (_isSome) {
+            *asValue() = *other.asValue();
+        } else {
+            _isSome = true;
+            new (asValue()) T(*other.asValue());
+        }
+    } else {
+        if (_isSome) {
+            _isSome = false;
+            asValue()->~T();
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+inline Option<T>& Option<T>::operator=(Option<T>&& other)
+{
+    if (other._isSome) {
+        if (_isSome) {
+            *asValue() = std::move(*other.asValue());
+        } else {
+            _isSome = true;
+            new (asValue()) T(std::move(*other.asValue()));
+        }
+        other._isSome = false;
+        other.asValue()->~T();
+    } else {
+        if (_isSome) {
+            _isSome = false;
+            asValue()->~T();
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+inline Option<T>& Option<T>::operator=(const T& value)
+{
+    if (_isSome) {
+        *asValue() = value;
+    } else {
+        _isSome = true;
+        new (asValue()) T(value);
+    }
+    return *this;
+}
+
+template <typename T>
+template <typename... A>
+inline void Option<T>::emplace(A&&... args)
+{
+    if (_isSome) {
+        *asValue() = T(std::forward<A>(args)...);
+    } else {
+        _isSome = true;
+        new (asValue()) T(std::forward<A>(args)...);
+    }
+}
+
+template <typename T>
+inline Option<T>& Option<T>::operator=(T&& value)
+{
+    if (_isSome) {
+        *asValue() = std::forward<T>(value);
+    } else {
+        _isSome = true;
+        new (asValue()) T(std::forward<T>(value));
+    }
+    return *this;
+}
+
+template <typename T>
+inline const T* Option<T>::operator->() const
+{
+    assert(_isSome);
+    return asValue();
+}
+
+template <typename T>
+inline T* Option<T>::operator->()
+{
+    assert(_isSome);
+    return asValue();
+}
+
+template <typename T>
+inline const T& Option<T>::operator*() const
+{
+    assert(_isSome);
+    return *asValue();
+}
+
+template <typename T>
+inline T& Option<T>::operator*()
+{
+    assert(_isSome);
+    return *asValue();
+}
+
+template <typename T>
+inline Option<T>::operator bool() const
+{
+    return _isSome;
+}
+
+template <typename T>
+inline bool operator==(const Option<T>& left, const Option<T>& right)
+{
+    if (left.isSome() && right.isSome()) {
+        return left.unwrap() == right.unwrap();
+    }
+    return left.isSome() == right.isSome();
+}
+}
---
+++ tests/Option.cpp
@@ -0,0 +1,285 @@
+#include "bmcl/Option.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+using namespace bmcl;
+
+TEST(OptionTest, createNone)
+{
+    Option<int> option;
+    EXPECT_FALSE(option.isSome());
+    EXPECT_TRUE(option.isNone());
+}
+
+static Option<double> noneTestFunc()
+{
+    return None;
+}
+
+TEST(OptionTest, createFromTag)
+{
+    Option<double> option = noneTestFunc();
+    EXPECT_FALSE(option.isSome());
+    EXPECT_TRUE(option.isNone());
+}
+
+
+TEST(OptionTest, createInt)
+{
+    Option<int> option(5);
+    EXPECT_TRUE(option.isSome());
+    EXPECT_FALSE(option.isNone());
+    EXPECT_EQ(5, option.unwrap());
+}
+
+TEST(OptionTest, createStringInPlace)
+{
+    std::string src("test string");
+    Option<std::string> option(InPlace, src, 1, 9);
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("est strin", option.unwrap());
+}
+
+
+TEST(OptionTest, createCopyString)
+{
+    std::string test = "copy string test";
+    Option<std::string> option(test);
+    EXPECT_TRUE(option.isSome());
+    EXPECT_FALSE(option.isNone());
+    EXPECT_EQ("copy string test", option.unwrap());
+}
+
+TEST(OptionTest, createMoveString)
+{
+    std::string test = "move string test";
+    Option<std::string> option(std::move(test));
+    EXPECT_TRUE(option.isSome());
+    EXPECT_FALSE(option.isNone());
+    EXPECT_EQ("move string test", option.unwrap());
+}
+
+TEST(OptionTest, unwrapMutable)
+{
+    Option<std::string> option("string test");
+    option.unwrap() = "123";
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("123", option.unwrap());
+}
+
+TEST(OptionTest, unwrapOrLvalue)
+{
+    Option<std::string> option1;
+    Option<std::string> option2("test");
+    EXPECT_EQ("result", option1.unwrapOr("result"));
+    EXPECT_EQ("test", option2.unwrapOr("result"));
+}
+
+TEST(OptionTest, unwrapOrRvalue)
+{
+    EXPECT_EQ("result", Option<std::string>().unwrapOr("result"));
+    EXPECT_EQ("test", Option<std::string>("test").unwrapOr("result"));
+}
+
+TEST(OptionTest, takeString)
+{
+    Option<std::string> option("take string test");
+    std::string newString = option.take();
+    EXPECT_FALSE(option.isSome());
+    EXPECT_TRUE(option.isNone());
+    EXPECT_EQ("take string test", newString);
+}
+
+TEST(OptionTest, copyConstructor)
+{
+    Option<std::string> oldOption("copy constructor test");
+    Option<std::string> newOption(oldOption);
+    EXPECT_TRUE(oldOption.isSome());
+    EXPECT_FALSE(oldOption.isNone());
+    EXPECT_EQ("copy constructor test", oldOption.unwrap());
+    EXPECT_TRUE(newOption.isSome());
+    EXPECT_FALSE(newOption.isNone());
+    EXPECT_EQ("copy constructor test", newOption.unwrap());
+}
+
+TEST(OptionTest, moveConstructor)
+{
+    Option<std::string> oldOption("move constructor test");
+    Option<std::string> newOption(std::move(oldOption));
+    EXPECT_FALSE(oldOption.isSome());
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isSome());
+    EXPECT_FALSE(newOption.isNone());
+    EXPECT_EQ("move constructor test", newOption.unwrap());
+}
+
+TEST(OptionTest, operatorEqNoneNone)
+{
+    Option<std::string> oldOption;
+    Option<std::string> newOption;
+    newOption = oldOption;
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isNone());
+}
+
+TEST(OptionTest, operatorEqNoneSome)
+{
+    Option<std::string> oldOption;
+    Option<std::string> newOption("some");
+    newOption = oldOption;
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isNone());
+}
+
+TEST(OptionTest, operatorEqSomeNone)
+{
+    Option<std::string> oldOption("some");
+    Option<std::string> newOption;
+    newOption = oldOption;
+    EXPECT_TRUE(oldOption.isSome());
+    EXPECT_TRUE(newOption.isSome());
+    EXPECT_EQ("some", oldOption.unwrap());
+    EXPECT_EQ("some", newOption.unwrap());
+}
+
+TEST(OptionTest, operatorEqSomeSome)
+{
+    Option<std::string> oldOption("some1");
+    Option<std::string> newOption("some2");
+    newOption = oldOption;
+    EXPECT_TRUE(oldOption.isSome());
+    EXPECT_TRUE(newOption.isSome());
+    EXPECT_EQ("some1", oldOption.unwrap());
+    EXPECT_EQ("some1", newOption.unwrap());
+}
+
+TEST(OptionTest, operatorEqNoneValue)
+{
+    Option<std::string> option;
+    std::string value("test value");
+    option = value;
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("test value", option.unwrap());
+}
+
+TEST(OptionTest, operatorEqSomeValue)
+{
+    Option<std::string> option("previous");
+    std::string value("next");
+    option = value;
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("next", option.unwrap());
+}
+
+TEST(OptionTest, operatorEqMoveNoneNone)
+{
+    Option<std::string> oldOption;
+    Option<std::string> newOption;
+    newOption = std::move(oldOption);
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isNone());
+}
+
+TEST(OptionTest, operatorEqMoveNoneSome)
+{
+    Option<std::string> oldOption;
+    Option<std::string> newOption("some");
+    newOption = std::move(oldOption);
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isNone());
+}
+
+TEST(OptionTest, operatorEqMoveSomeNone)
+{
+    Option<std::string> oldOption("some");
+    Option<std::string> newOption;
+    newOption = std::move(oldOption);
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isSome());
+    EXPECT_EQ("some", newOption.unwrap());
+}
+
+TEST(OptionTest, operatorEqMoveSomeSome)
+{
+    Option<std::string> oldOption("some1");
+    Option<std::string> newOption("some2");
+    newOption = std::move(oldOption);
+    EXPECT_TRUE(oldOption.isNone());
+    EXPECT_TRUE(newOption.isSome());
+    EXPECT_EQ("some1", newOption.unwrap());
+}
+
+TEST(OptionTest, operatorEqMoveNoneValue)
+{
+    Option<std::string> option;
+    std::string value("test value");
+    option = std::move(value);
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("test value", option.unwrap());
+}
+
+TEST(OptionTest, operatorEqMoveSomeValue)
+{
+    Option<std::string> option("previous");
+    std::string value("next");
+    option = std::move(value);
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("next", option.unwrap());
+}
+
+TEST(OptionTest, operatorBool)
+{
+    Option<std::string> none;
+    Option<std::string> some("some");
+    EXPECT_TRUE(some);
+    EXPECT_FALSE(none);
+}
+
+TEST(OptionTest, operatorEqEq)
+{
+    Option<std::string> none1;
+    Option<std::string> none2;
+    Option<std::string> some1("some");
+    Option<std::string> some2("some");
+    Option<std::string> some3("emos");
+    EXPECT_TRUE(none1 == none2);
+    EXPECT_TRUE(none2 == none1);
+    EXPECT_TRUE(some1 == some2);
+    EXPECT_TRUE(some2 == some1);
+    EXPECT_FALSE(none1 == some1);
+    EXPECT_FALSE(some2 == none2);
+    EXPECT_FALSE(some1 == some3);
+    EXPECT_FALSE(some3 == some2);
+}
+
+TEST(OptionTest, operatorDeref)
+{
+    Option<std::string> option("some");
+    EXPECT_TRUE(option.isSome());
+    EXPECT_STREQ("some", option->c_str());
+    option->append(" test");
+    EXPECT_STREQ("some test", option->c_str());
+}
+
+TEST(OptionTest, operatorInd)
+{
+    Option<std::string> option("some");
+    EXPECT_TRUE(option.isSome());
+    EXPECT_STREQ("some", (*option).c_str());
+    (*option).append(" test");
+    EXPECT_STREQ("some test", (*option).c_str());
+}
+
+TEST(OptionTest, emplace)
+{
+    Option<std::string> option;
+    EXPECT_FALSE(option.isSome());
+    option.emplace("some1");
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("some1", option.unwrap());
+    option.emplace("2some");
+    EXPECT_TRUE(option.isSome());
+    EXPECT_EQ("2some", option.unwrap());
+}
