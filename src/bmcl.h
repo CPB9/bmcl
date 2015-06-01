395a52ccbbdff940471babf20d6c8c487d57ebd7
dndanik <dndanik@gmail.com>
2015-06-01 22:35:41 +0300

    Add top level header

---
+++ src/bmcl.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2014 CPB9 team. See the COPYRIGHT file at the top-level directory.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "bmcl/Alloca.h"
+#include "bmcl/Assert.h"
+#include "bmcl/Buffer.h"
+#include "bmcl/ColorStream.h"
+#include "bmcl/Either.h"
+#include "bmcl/Endian.h"
+#include "bmcl/Logging.h"
+#include "bmcl/MemReader.h"
+#include "bmcl/MemWriter.h"
+#include "bmcl/Option.h"
+#include "bmcl/Panic.h"
+#include "bmcl/Reader.h"
+#include "bmcl/Result.h"
+#include "bmcl/RingArray.h"
+#include "bmcl/RingBucket.h"
+#include "bmcl/RingBuffer.h"
+#include "bmcl/Utils.h"
+#include "bmcl/Writer.h"
